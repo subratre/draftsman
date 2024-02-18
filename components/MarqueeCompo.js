2227d51c1ca239c8bac4170ac9ce80bbf72cf3db
subrat ranjan panda <subrat.panda@muvi.com>
2024-02-18 17:29:10 +0530

    all commit

--- components/MarqueeCompo.js
+++ components/MarqueeCompo.js
@@ -1,8 +1,21 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
 const MarqueeCompo = () => {
+  const [color, setColor] = useState("black");
+  const [isBlue, setIsBlue] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsBlue((prevIsBlue) => !prevIsBlue);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+  useEffect(() => {
+    setColor(isBlue ? "blue" : "#fff");
+  }, [isBlue]);
   return (
     <Marquee pauseOnHover={true} style={{ flex: 2, marginLeft: 80 }}>
       <p
@@ -13,8 +26,15 @@ const MarqueeCompo = () => {
           fontWeight: "bold",
         }}
       >
-        <Link href="/hire-a-professional-reviewer" style={{ color: "#fff" }}>
-          Hire a Professional Reviewer
+        <Link
+          href="/hire-a-professional-reviewer"
+          style={{
+            color,
+            transition: "color 1s",
+            textDecoration: "underline !important",
+          }}
+        >
+          Hire a Professional Reviewer{" "}
         </Link>
       </p>{" "}
     </Marquee>
