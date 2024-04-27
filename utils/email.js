5c2363040506211bb9a74a2b549ccea982052186
subrat ranjan panda <subrat.panda@muvi.com>
2024-04-27 06:34:16 +0530

    new email config

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.draftsmans.com",
+  host: "smtp-mail.outlook.com",
   port: 587,
   secure: false,
   auth: {
